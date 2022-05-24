b00ee96aa7651cbc3a153e1f2e89cf7da95d4674
yoongyum <imshreg@naver.com>
2022-05-24 14:28:09 +0900

    feat: Create Member Entity
    Column:
    seq, id, password, name, role, intro, age, datetime

--- src/main/java/com/example/boardpractice/domain/Member.java
+++ src/main/java/com/example/boardpractice/domain/Member.java
@@ -1,17 +1,39 @@
 package com.example.boardpractice.domain;
 
-import lombok.Data;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Member {
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
-    private String id;
-    private String password;
-    private String name;
-    private String role;
+    
+    @Id//Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;  //고유 번호
+
+    @Column(length= 20, unique = true)
+    private String id;  //회원 아이디 varchar(20)
+
+    @Column(length = 13, nullable = false)
+    private String password;    //회원 비밀번호 varchar(13)
+
+    @Column(length = 10, nullable = false)
+    private String name;    //회원 이름 varchar(10)
+
+    @Column
+    private String role;    //권한 USER or ADMIN
+
+    @Column(length = 200)
+    private String intro;   //자기소개 varchar(200)
+
+    @Column
+    private String age;     //나이대
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datetime; //생성 날짜
 }
--- src/main/java/com/example/boardpractice/repository/MemberRepository.java
+++ src/main/java/com/example/boardpractice/repository/MemberRepository.java
@@ -1,12 +1,14 @@
 package com.example.boardpractice.repository;
 
 import com.example.boardpractice.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member,Long> {
     Member save(Member member);
+    Optional<Member> findBySeq(Long seq);
     Optional<Member> findById(String id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
