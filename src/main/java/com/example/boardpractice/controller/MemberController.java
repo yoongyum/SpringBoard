dc46480deeb1f36f9cc37f5a219dd0945bff2596
yoongyum <imshreg@naver.com>
2022-05-25 13:28:38 +0900

    refactoring: Add final to MembserService field

--- src/main/java/com/example/boardpractice/controller/MemberController.java
+++ src/main/java/com/example/boardpractice/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MemberController {
 
-    private MemberService memberService;
+    private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService){
