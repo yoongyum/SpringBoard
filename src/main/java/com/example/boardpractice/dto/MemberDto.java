b738b4033e59c816899c461b34343192da6fead1
yoongyum <imshreg@naver.com>
2022-05-25 21:22:15 +0900

    feat: Modify MemberDto for Validation
    email, password, name

--- src/main/java/com/example/boardpractice/dto/MemberDto.java
+++ src/main/java/com/example/boardpractice/dto/MemberDto.java
@@ -10,17 +10,19 @@ import java.time.LocalDateTime;
 public class MemberDto {
 
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
-    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
+    @Pattern(regexp = "^.{2,10}$", message = "닉네임은 2~10자리여야 합니다.")
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String name;
 
     private String role;
     private String intro;
+
     private String age;
     private LocalDateTime createDate;
 }
