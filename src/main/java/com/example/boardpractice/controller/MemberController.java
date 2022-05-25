8448f2f68adb9d7e61f9f7ea802e76d491907287
yoongyum <imshreg@naver.com>
2022-05-26 01:42:38 +0900

    feat: Add validatorBinder
    Bind emailValidator, nicknameValidator

--- src/main/java/com/example/boardpractice/controller/MemberController.java
+++ src/main/java/com/example/boardpractice/controller/MemberController.java
@@ -2,29 +2,38 @@ package com.example.boardpractice.controller;
 
 import com.example.boardpractice.dto.MemberDto;
 import com.example.boardpractice.service.MemberService;
+import com.example.boardpractice.validator.CheckMemberEmailValidator;
+import com.example.boardpractice.validator.CheckMemberNameValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
+    private final CheckMemberEmailValidator checkMemberEmailValidator;
+    private final CheckMemberNameValidator checkMemberNameValidator;
 
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
+    @InitBinder
+    public void validatorBinder(WebDataBinder binder){
+        binder.addValidators(checkMemberEmailValidator);
+        binder.addValidators(checkMemberNameValidator);
     }
 
     @GetMapping("/member/insertMember")
-    public ModelAndView create(@Valid MemberDto memberDto, Model model) {
+    public ModelAndView create(@Valid MemberDto memberDto, Errors errors ,Model model) {
         model.addAttribute("memberDto", memberDto);
         return new ModelAndView("/member/insertMember");
     }
@@ -36,7 +45,6 @@ public class MemberController {
         if (errors.hasErrors()) {
             //회원가입 실패 시 입력 데이터값을 유지
             model.addAttribute("memberDto", memberDto);
-            System.out.println("오류 발생");
             //유효성 통과 못한 필드와 에러메세지 핸들링
             Map<String, String> validationResult = memberService.validationHandler(errors);
 
