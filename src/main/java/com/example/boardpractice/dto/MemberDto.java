e71cce5b94d06cf4ad1032581f8edd4cbe74af72
yoongyum <imshreg@naver.com>
2022-05-25 13:57:34 +0900

    feat: Add Validation to DTO
    email, password, name

--- src/main/java/com/example/boardpractice/dto/MemberDto.java
+++ src/main/java/com/example/boardpractice/dto/MemberDto.java
@@ -2,14 +2,23 @@ package com.example.boardpractice.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 @Data
 public class MemberDto {
-    private Long seq;
+
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String name;
+
     private String role;
     private String intro;
     private String age;
