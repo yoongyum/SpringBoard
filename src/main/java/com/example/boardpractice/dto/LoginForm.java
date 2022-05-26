66cdba9d794db72ff29f5c6279f9afb35ac19554
yoongyum <imshreg@naver.com>
2022-05-26 15:28:10 +0900

    feat: Create LoginForm.java as DTO

---
+++ src/main/java/com/example/boardpractice/dto/LoginForm.java
@@ -0,0 +1,11 @@
+package com.example.boardpractice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginForm {
+    private String email;
+    private String password;
+}
