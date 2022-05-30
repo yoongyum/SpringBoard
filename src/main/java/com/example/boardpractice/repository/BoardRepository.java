ed51e1ccb6b24e453569966c300980209cded39a
yoongyum <imshreg@naver.com>
2022-05-30 16:16:45 +0900

    feat: Create BoardRepository

---
+++ src/main/java/com/example/boardpractice/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package com.example.boardpractice.repository;
+
+import com.example.boardpractice.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+    Board save(Board board);
+    Optional<Board> findBySeq(Long seq);
+}
