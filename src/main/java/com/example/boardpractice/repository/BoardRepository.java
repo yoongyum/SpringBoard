f9fc3e9128f9bd9c0373621cdd8bf38d8827d4f9
yoongyum <imshreg@naver.com>
2022-05-31 16:45:49 +0900

    feat: Create deleteBySeq

--- src/main/java/com/example/boardpractice/repository/BoardRepository.java
+++ src/main/java/com/example/boardpractice/repository/BoardRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface BoardRepository extends JpaRepository<Board,Long> {
     Board save(Board board);
     Optional<Board> findBySeq(Long seq);
+    void deleteBySeq(Long seq);
 }
