d7e86994946f22e8202b72424ffa4299e5975a4b
yoongyum <imshreg@naver.com>
2022-06-03 00:26:59 +0900

    feat: Change MemberDto role type

--- src/main/java/com/example/boardpractice/dto/MemberDto.java
+++ src/main/java/com/example/boardpractice/dto/MemberDto.java
@@ -1,13 +1,23 @@
 package com.example.boardpractice.dto;
 
+import com.example.boardpractice.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class MemberDto {
+
+    private Long seq;
 
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력값입니다.")
@@ -20,7 +30,7 @@ public class MemberDto {
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String name;
 
-    private String role;
+    private Role role;
     private String intro;
 
     private String age;
