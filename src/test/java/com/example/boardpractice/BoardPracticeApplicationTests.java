9becd135aa297a02ff8e1227562324aa6ff9d1e2
yoongyum <imshreg@naver.com>
2022-06-03 00:31:22 +0900

    clean: Delete insertDummies of Test fuc

--- src/test/java/com/example/boardpractice/BoardPracticeApplicationTests.java
+++ src/test/java/com/example/boardpractice/BoardPracticeApplicationTests.java
@@ -29,22 +29,7 @@ class BoardPracticeApplicationTests {
     @Autowired
     BoardService boardService;
 
-//    @Test
-    @Order(1)
-    public void insertDummies() {//회원 등록
-        IntStream.rangeClosed(1, 10).forEach(i -> {
-            Member member = Member.MemberBuilder()
-                    .email("GuestUser" + i)
-                    .password("1234")
-                    .name("usr" + i)
-                    .role("USER")
-                    .intro("잘부탁드립니다.." + i)
-                    .age("20대")
-                    .createDate(LocalDateTime.now())
-                    .build();
-            memberRepository.save(member);
-        });
-    }
+
 
 //    @Test
     @Order(2)
