78f2d06557f79857462e5bddcd87634432e198d4
yoongyum <imshreg@naver.com>
2022-06-03 00:32:40 +0900

    feat: Change Role type

--- src/main/java/com/example/boardpractice/domain/Member.java
+++ src/main/java/com/example/boardpractice/domain/Member.java
@@ -1,6 +1,8 @@
 package com.example.boardpractice.domain;
 
+import com.example.boardpractice.auth.dto.SessionMember;
 import com.example.boardpractice.dto.MemberDto;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
@@ -21,14 +23,15 @@ public class Member {
     @Column(length= 50, unique = true)
     private String email;  //회원 아이디 varchar(50)
 
-    @Column(length = 13, nullable = false)
-    private String password;    //회원 비밀번호 varchar(13)
+//    @Column(length = 13, nullable = false)
+//    private String password;    //회원 비밀번호 varchar(13)
 
     @Column(length = 20, nullable = false)
     private String name;    //회원 이름 varchar(20)
 
-    @Column
-    private String role;    //권한 USER or ADMIN
+    @Enumerated
+    @Column(nullable = false)
+    private Role role;    //권한 USER or ADMIN
 
     @Column(length = 200)
     private String intro;   //자기소개 varchar(200)
@@ -39,19 +42,26 @@ public class Member {
     @Column
     private String age;     //나이대
 
-    @CreatedDate
-    @Column(updatable = false, nullable = false)
-    private LocalDateTime createDate; //생성 날짜
+//    @CreatedDate
+//    @Column(updatable = false, nullable = false)
+//    private LocalDateTime createDate; //생성 날짜
 
-    public static MemberBuilder builder(MemberDto memberDto){
+//    public static MemberBuilder builder(MemberDto memberDto){
+//        return MemberBuilder()
+//                .email(memberDto.getEmail())
+//                .password(memberDto.getPassword())
+//                .name(memberDto.getName())
+//                .role(memberDto.getRole())
+//                .intro(memberDto.getIntro())
+//                .age(memberDto.getAge())
+//                .createDate(LocalDateTime.now());
+//    }
+    public static MemberBuilder builder(SessionMember sessionMember){
         return MemberBuilder()
-                .email(memberDto.getEmail())
-                .password(memberDto.getPassword())
-                .name(memberDto.getName())
-                .role(memberDto.getRole())
-                .intro(memberDto.getIntro())
-                .age(memberDto.getAge())
-                .createDate(LocalDateTime.now());
+                .name(sessionMember.getName())
+                .email(sessionMember.getEmail())
+                .picture(sessionMember.getPicture())
+                .role(Role.USER);
     }
 
     public Member update(String name, String picture){
@@ -59,5 +69,8 @@ public class Member {
         this.picture = picture;
 
         return this;
+    }
+    public String getRoleKey(){
+        return this.role.getKey();
     }
 }
