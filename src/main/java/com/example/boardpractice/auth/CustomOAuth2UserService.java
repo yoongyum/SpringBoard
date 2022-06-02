2db1b3f0368f97c18b0e94b918d57c950e8f848d
yoongyum <imshreg@naver.com>
2022-06-02 22:11:36 +0900

    feat: Create CustomOAuth2UserService

---
+++ src/main/java/com/example/boardpractice/auth/CustomOAuth2UserService.java
@@ -0,0 +1,55 @@
+package com.example.boardpractice.auth;
+
+import com.example.boardpractice.auth.dto.OAuthAttributes;
+import com.example.boardpractice.auth.dto.SessionMember;
+import com.example.boardpractice.domain.Member;
+import com.example.boardpractice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberRepository memberRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest,OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        //OAuth2 서비스 id(카카오)
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        //OAuth2 로그인 진행 시 키가 되는 필드 값(PK)
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        //OAuth2UserService
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        Member member = saveOrUpdate(attributes);
+        httpSession.setAttribute("member", new SessionMember(member));  //SessionMember (직렬화된 dto)
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(member.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    //회원 생성 및 수정 서비스 로직
+    private Member saveOrUpdate(OAuthAttributes attributes){
+        Member member = memberRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return memberRepository.save(member);
+    }
+}
