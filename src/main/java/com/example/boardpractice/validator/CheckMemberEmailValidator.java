1201c68c10f51a711bfdbc88942bc2eca41e5ffc
yoongyum <imshreg@naver.com>
2022-06-13 15:22:48 +0900

    fix: memberDto->sessionMember로 변경

--- src/main/java/com/example/boardpractice/validator/CheckMemberEmailValidator.java
+++ src/main/java/com/example/boardpractice/validator/CheckMemberEmailValidator.java
@@ -1,5 +1,6 @@
 package com.example.boardpractice.validator;
 
+import com.example.boardpractice.auth.dto.SessionMember;
 import com.example.boardpractice.dto.MemberDto;
 import com.example.boardpractice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,12 @@ import org.springframework.validation.Errors;
 
 @RequiredArgsConstructor
 @Component
-public class CheckMemberEmailValidator extends AbstractValidator<MemberDto>{
+public class CheckMemberEmailValidator extends AbstractValidator<SessionMember>{
 
     private final MemberRepository memberRepository;
 
     @Override
-    protected void doValidate(MemberDto dto, Errors errors) {
+    protected void doValidate(SessionMember dto, Errors errors) {
         if(memberRepository.existsMemberByEmail(dto.getEmail())){
             errors.rejectValue("email","이메일 중복 오류", "이미 사용중인 이메일 주소 입니다.");
         }
--- src/main/java/com/example/boardpractice/validator/CheckMemberNameValidator.java
+++ src/main/java/com/example/boardpractice/validator/CheckMemberNameValidator.java
@@ -1,5 +1,6 @@
 package com.example.boardpractice.validator;
 
+import com.example.boardpractice.auth.dto.SessionMember;
 import com.example.boardpractice.dto.MemberDto;
 import com.example.boardpractice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,12 @@ import org.springframework.validation.Errors;
 
 @RequiredArgsConstructor
 @Component
-public class CheckMemberNameValidator extends AbstractValidator<MemberDto>{
+public class CheckMemberNameValidator extends AbstractValidator<SessionMember>{
 
     private final MemberRepository memberRepository;
 
     @Override
-    protected void doValidate(MemberDto dto, Errors errors) {
+    protected void doValidate(SessionMember dto, Errors errors) {
         if(memberRepository.existsMemberByName(dto.getName())){
             errors.rejectValue("name","닉네임 중복 오류", "이미 사용중인 닉네임입니다.");
         }
