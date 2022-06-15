069163c49041883a7c1b9e156046743c9b9fc748
yoongyum <imshreg@naver.com>
2022-06-15 14:39:55 +0900

    feat: memberDto인 SessionMember에도 게시글 리스트 추가

--- src/main/java/com/example/boardpractice/auth/dto/SessionMember.java
+++ src/main/java/com/example/boardpractice/auth/dto/SessionMember.java
@@ -1,21 +1,29 @@
 package com.example.boardpractice.auth.dto;
 
+import com.example.boardpractice.domain.Board;
 import com.example.boardpractice.domain.Member;
 import com.example.boardpractice.domain.Role;
+import com.example.boardpractice.dto.BoardDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class SessionMember implements Serializable {
 
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
+//    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+//    @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
 
 //    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
@@ -28,16 +36,22 @@ public class SessionMember implements Serializable {
     private String picture;
 
     private Role role;
+
     private String intro;
 
     private String age;
     private LocalDateTime createDate;
 
+    //게시글 리스트
+    private List<Board> boards = new ArrayList<>();
+
+    //member Entity를 파라미터로 갖는 세션멤버 DTO생성자
     public SessionMember(Member member) {
         this.name = member.getName();
         this.email = member.getEmail();
         this.picture = member.getPicture();
         this.intro = member.getIntro();
+        this.boards = member.getBoards();
         this.role = member.getRole();
     }
 }
