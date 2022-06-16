80e1c7d8a0f9c2b4fd19201ba468ef2e99926649
yoongyum <imshreg@naver.com>
2022-06-16 13:49:26 +0900

    feat: 회원상세뷰에서 모달 보이게하는 함수 추가

--- src/main/resources/static/js/info.js
+++ src/main/resources/static/js/info.js
@@ -2,3 +2,6 @@ function edit()  {
     document.getElementById("viewInfoForm").style.display = 'none';
     document.getElementById("editInfoForm").style.display = 'block';
 }
+function delete_member(){
+    document.getElementById("viewModalContainer").style.display = 'block';
+}
