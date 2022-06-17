af6d07234cd86ca0826efec6beb254918191a86f
yoongyum <imshreg@naver.com>
2022-06-17 15:18:37 +0900

    feat: BoardDto에 일단 comments리스트 Entity 갖고있음 수정해야함

--- src/main/java/com/example/boardpractice/dto/BoardDto.java
+++ src/main/java/com/example/boardpractice/dto/BoardDto.java
@@ -1,6 +1,7 @@
 package com.example.boardpractice.dto;
 
 import com.example.boardpractice.auth.dto.SessionMember;
+import com.example.boardpractice.domain.Comment;
 import com.example.boardpractice.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -17,6 +20,7 @@ public class BoardDto {
     private Long seq;
     private String title;
     private Member member;
+    private List<Comment> comments = new ArrayList<>();
     private String content;
     private LocalDateTime createDate;
     private LocalDateTime modifiedDate;
