8e1e4ff1e0b8d17e5df487bc8ce2a14e174ff09e
yoongyum <imshreg@naver.com>
2022-06-21 13:18:46 +0900

    feat: 회원삭제 모달창을 통해서 작동

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,12 +1,4 @@
 function disappearModal(){
     document.getElementById("viewModalContainer").style.display = 'none';
 }
-function initMouseClickEvent(){
-    $(document).mouseup(function(e){
-        var sch_container = $("modal-view");
-        if(sch_container.has(e.target).length ===0)
-            sch_container.hide();
 
-    });
-
-}
