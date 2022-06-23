8520d8cf2de86930a6ab3a3a5d5362f025175ba2
yoongyum <imshreg@naver.com>
2022-06-23 16:47:18 +0900

    feat: 댓글 유효성체크 함수와 resize함수 js파일로 분리

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -10,3 +10,24 @@ function cancel(seq)  {
     view.style.display = 'block';
     edit.style.display = 'none';
 }
+
+//댓글 입력창 동적크기 변환
+function resize(obj){
+    // const hei = window.getComputedStyle(obj).height
+    if (obj.scrollHeight >= 90){
+        obj.style.height = "0px";
+        obj.style.height = (16 + obj.scrollHeight) + "px";
+    }
+}
+
+//댓글 유효성 검사
+function checkForm(frm,contentId, errorId) {
+    const form = document.forms.namedItem(frm);
+    const content = document.getElementById(contentId);
+    if (content.value === "") {
+        document.getElementById(errorId).style.display = "block";
+    } else {
+        form.submit();
+    }
+    return false;
+}
