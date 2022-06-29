81a51477536f3c957affc2a415f9e24b6575bdc7
yoongyum <imshreg@naver.com>
2022-06-29 14:18:09 +0900

    feat: BoardDto에 좋아요 리스트 추가

--- src/main/java/com/example/boardpractice/dto/BoardDto.java
+++ src/main/java/com/example/boardpractice/dto/BoardDto.java
@@ -2,6 +2,7 @@ package com.example.boardpractice.dto;
 
 import com.example.boardpractice.auth.dto.SessionMember;
 import com.example.boardpractice.domain.Comment;
+import com.example.boardpractice.domain.Likes;
 import com.example.boardpractice.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,7 @@ public class BoardDto {
     private String title;
     private Member member;
     private List<Comment> comments = new ArrayList<>();
+    private List<Likes> likes = new ArrayList<>();
     private String content;
     private LocalDateTime createDate = LocalDateTime.now();
     private LocalDateTime modifiedDate;
