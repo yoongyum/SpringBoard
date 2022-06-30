f361eeab38f80bf4b7a2177cfe4c9255be4ea990
yoongyum <imshreg@naver.com>
2022-06-30 17:30:51 +0900

    feat: 댓글 Dto에 boardSeq추가

--- src/main/java/com/example/boardpractice/dto/CommentDto.java
+++ src/main/java/com/example/boardpractice/dto/CommentDto.java
@@ -13,7 +13,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 public class CommentDto {
-
+    Long boardSeq; //댓글이 위치한 게시글 번호
+    
     @NotBlank(message = "댓글에 아무 내용이 없습니다.")
     String commentContent;
     private LocalDateTime createDate = LocalDateTime.now();
