1789fa4da150fb4d4be4345c645a9a37da900726
yoongyum <imshreg@naver.com>
2022-07-08 00:28:27 +0900

    feat: Create viewLoginForm()

--- src/main/java/com/example/boardpractice/controller/LoginController.java
+++ src/main/java/com/example/boardpractice/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.example.boardpractice.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,5 +29,10 @@ public class LoginController {
         }
         model.addAttribute("member",memberDto);
         return new ModelAndView("redirect:/");
+    }
+
+    @GetMapping("/login/loginForm")
+    public String viewLoginForm(){
+        return "/login/loginForm";
     }
 }
